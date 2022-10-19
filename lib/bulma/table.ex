f1595f9fa101253495cc58e74ebc381a9432f29a
Thorsten Deinert <thorsten.deinert@udo.edu>
2022-10-19 10:09:20 +0200

    Table component defines attr and slots, and uses lv 0.18 features

--- lib/bulma/table.ex
+++ lib/bulma/table.ex
@@ -1,81 +1,74 @@
 defmodule Bulma.Table do
-  use Phoenix.Component
+  @moduledoc """
 
+  """
+  use Phoenix.Component
   import Bulma.Helpers
   import Bulma.Label, only: [label: 1]
 
-  @properties [
-    data: [],
-    column: [],
-    footer: [],
-    header: [],
-    column_header: [],
-    style: nil,
-    fullwidth: nil
-  ]
+  attr :data, :list,  default: [], doc: "The data that is used to generate rows"
+  attr :style, :string, default: nil
+  attr :fullwidth, :boolean, default: false
+  slot :column, doc: "Columns with header label" do
+    attr :field, :atom, doc: "Columns data field"
+    attr :header, :string, doc: "Column header label"
+    attr :icon, :string, doc: "Column header icon"
+  end
+  slot :header, doc: "If not given, no headers are shown. Can be used to define a template for a column header"
+  slot :footer, doc: "If not given, no footer row is shown. Can be used to define a template for a column footer"
+  slot :column_header, doc: "Columns with row label" do
+    attr :field, :atom, doc: "data field for row header"
+  end
+
+
   def table(assigns) do
     assigns =
       assigns
-      |> assign_defaults(@properties)
+     # |> assign_defaults(@properties)
       |> assign_class(["table", is(:style), is(:fullwidth)])
-      |> set_attributes_from_assigns(exclude: Keyword.keys(@properties))
+      #|> set_attributes_from_assigns(exclude: Keyword.keys(@properties))
 
     ~H"""
-    <table {@attributes}>
-      <thead>
-        <%= for header <- @header do %>
-          <tr>
-            <%= for col <- @column_header ++ @column do %>
-              <th>
-                <%= if header[:inner_block] do %>
-                  <%= render_slot(header, col) %>
-                <% else %>
-                  <.label label={col[:header] || col[:field]} {col} />
-                <% end %>
-              </th>
+    <table class={@class}>
+      <thead :if={has_slot?(@header)}>
+        <tr :for={header <- @header}>
+          <th :for={col <- @column_header ++ @column}>
+            <%= if header[:inner_block] do %>
+              <%= render_slot(header, col) %>
+            <% else %>
+              <.label label={col[:header] || col[:field]} {col} />
             <% end %>
-          </tr>
-        <% end %>
+          </th>
+        </tr>
       </thead>
       <tbody>
-        <%= for {item, row_idx} <- Enum.with_index(@data, 1) do %>
-          <tr>
-            <%= for col <- @column_header do %>
-
-              <th>
-                <%= if col[:inner_block] do %>
-                  <%= render_slot(col, %{item: item, idx: row_idx, field: Map.get(item, col[:field])}) %>
-                <% else %>
-                  <.label label={Map.get(item, col[:field], row_idx)} />
-                <% end %>
-              </th>
+        <tr :for={{item, row_idx} <- Enum.with_index(@data, 1)}>
+          <th :for={col <- @column_header}>
+            <%= if col[:inner_block] do %>
+              <%= render_slot(col, %{item: item, idx: row_idx, field: Map.get(item, col[:field])}) %>
+            <% else %>
+              <.label label={Map.get(item, col[:field], row_idx)} />
             <% end %>
-            <%= for col <- @column do %>
-            <td>
-              <%= if col[:inner_block] do %>
-                <%= render_slot(col, %{item: item, idx: row_idx, field: Map.get(item, col[:field])}) %>
-              <% else %>
-                <.label label={Map.get(item, col.field)} />
-              <% end %>
-            </td>
+          </th>
+          <td :for={col <- @column}>
+            <%= if col[:inner_block] do %>
+              <%= render_slot(col, %{item: item, idx: row_idx, field: Map.get(item, col[:field])}) %>
+            <% else %>
+              <.label label={Map.get(item, col.field)} />
             <% end %>
-          </tr>
-        <% end %>
+          </td>
+        </tr>
       </tbody>
-      <tfoot>
-      <%= for footer <- @footer do %>
-        <tr>
-          <%= for col <- @column_header ++ @column do %>
-            <th>
+      <tfoot :if={has_slot?(@footer)}>
+        <tr :for={footer <- @footer}>
+          <th :for={col <- @column_header ++ @column} >
             <%= if footer[:inner_block] do %>
               <%= render_slot(footer, col) %>
-             <% else %>
-                <.label label={col[:header] || col[:field]} {col} />
-              <% end %>
-            </th>
-          <% end %>
+            <% else %>
+              <.label label={col[:header] || col[:field]} {col} />
+            <% end %>
+          </th>
         </tr>
-      <% end %>
       </tfoot>
     </table>
     """
